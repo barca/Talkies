94914a3ad5d63f3fc90284dbf66a6632825de24a
barca <tharden@wesleyan.edu>
2015-07-24 14:14:23 +0300

    adding backend files for nanodegree

---
+++ media.py
@@ -0,0 +1,16 @@
+class movie:
+	def __init__(self, title, date, poster, desc, trailer):
+		self.title = title
+		self.release_date = date
+		self.poster_image_url = poster
+		self.desc = desc
+		self.trailer_youtube_url = trailer
+		self.big_pic = None
+	def __init__(self, title, release_date, poster, desc, trailer, big_pic):
+		self.title = title
+		self.release_date = date
+		self.poster_image_url = poster
+		self.desc = desc
+		self.trailer_youtube_url = trailer
+		self.big_pic = big_pic
+		
