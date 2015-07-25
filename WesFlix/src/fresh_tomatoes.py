7ae152ff19c41e93d363ca15ae0395b2cf896890
barca <tharden@wesleyan.edu>
2015-07-25 14:29:53 +0300

    configuring backend to nano specs

--- WesFlix/src/fresh_tomatoes.py
+++ WesFlix/src/fresh_tomatoes.py
@@ -6,148 +6,187 @@ import re
 # Styles and scripting for the page
 main_page_head = '''
 <head>
+    <html lang="en">
+  <head>
     <meta charset="utf-8">
-    <title>Fresh Tomatoes!</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
 
-    <!-- Bootstrap 3 -->
-    <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap-theme.min.css">
-    <script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
-    <script src="https://netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
-    <style type="text/css" media="screen">
-        body {
-            padding-top: 80px;
-        }
-        #trailer .modal-dialog {
-            margin-top: 200px;
-            width: 640px;
-            height: 480px;
-        }
-        .hanging-close {
-            position: absolute;
-            top: -12px;
-            right: -12px;
-            z-index: 9001;
-        }
-        #trailer-video {
-            width: 100%;
-            height: 100%;
-        }
-        .movie-tile {
-            margin-bottom: 20px;
-            padding-top: 20px;
-        }
-        .movie-tile:hover {
-            background-color: #EEE;
-            cursor: pointer;
-        }
-        .scale-media {
-            padding-bottom: 56.25%;
-            position: relative;
-        }
-        .scale-media iframe {
-            border: none;
-            height: 100%;
-            position: absolute;
-            width: 100%;
-            left: 0;
-            top: 0;
-            background-color: white;
-        }
-    </style>
-    <script type="text/javascript" charset="utf-8">
-        // Pause the video when the modal is closed
-        $(document).on('click', '.hanging-close, .modal-backdrop, .modal', function (event) {
-            // Remove the src so the player itself gets removed, as this is the only
-            // reliable way to ensure the video stops playing in IE
-            $("#trailer-video-container").empty();
-        });
-        // Start playing the video whenever the trailer modal is opened
-        $(document).on('click', '.movie-tile', function (event) {
-            var trailerYouTubeId = $(this).attr('data-trailer-youtube-id')
-            var sourceUrl = 'http://www.youtube.com/embed/' + trailerYouTubeId + '?autoplay=1&html5=1';
-            $("#trailer-video-container").empty().append($("<iframe></iframe>", {
-              'id': 'trailer-video',
-              'type': 'text-html',
-              'src': sourceUrl,
-              'frameborder': 0
-            }));
-        });
-        // Animate in the movies when the page loads
-        $(document).ready(function () {
-          $('.movie-tile').hide().first().show("fast", function showNext() {
-            $(this).next("div").show("fast", showNext);
-          });
-        });
-    </script>
-</head>
+    <title>WesFlix</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="../static/lib/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="../static/assets/js/ie-emulation-modes-warning.js"></script>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+    <!-- Custom styles for this template -->
+    <link href="../static/css/carousel.css" rel="stylesheet">
+    <link href="../static/css/styles.css" rel="stylesheet">
+  </head>
 '''
 
 
 # The main page layout and title bar
 main_page_content = '''
-<!DOCTYPE html>
-<html lang="en">
-  <body>
-    <!-- Trailer Video Modal -->
-    <div class="modal" id="trailer">
-      <div class="modal-dialog">
-        <div class="modal-content">
-          <a href="#" class="hanging-close" data-dismiss="modal" aria-hidden="true">
-            <img src="https://lh5.ggpht.com/v4-628SilF0HtHuHdu5EzxD7WRqOrrTIDi_MhEG6_qkNtUK5Wg7KPkofp_VJoF7RS2LhxwEFCO1ICHZlc-o_=s0#w=24&h=24"/>
-          </a>
-          <div class="scale-media" id="trailer-video-container">
+<body>
+    <div class="navbar-wrapper">
+      <div class="container">
+
+        <nav class="navbar navbar-inverse navbar-static-top">
+          <div class="container">
+            <div class="navbar-header">
+              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+              <a class="navbar-brand" href="#">WesFlix</a>
+            </div>
+            <div id="navbar" class="navbar-collapse collapse">
+              <ul class="nav navbar-nav">
+                <li class="active"><a href="#">Home</a></li>
+                <li><a href="template.html">Template</a></li>
+                <li><a href="contact.html">Contact</a></li>
+                <li><a href="calendar.html">Calendar</a></li>
+              </ul>
+            <div class="input-group" style="padding-top: 8px">
+              <input type="text" class="form-control" id="input" placeholder="Search for a specific trailer...">
+              <span class="input-group-btn">
+                <button class="btn btn-default" type="button" id="input">Go! </button>
+              </span>
+            </div>
           </div>
-        </div>
+        </nav>
+
       </div>
+    </div>
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+      <!-- Indicators -->
+      <ol class="carousel-indicators">
+        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+        <li data-target="#myCarousel" data-slide-to="1"></li>
+        <li data-target="#myCarousel" data-slide-to="2"></li>
+        <li data-target="#myCarousel" data-slide-to="3"></li>
+      </ol>
+      <div class="carousel-inner" role="listbox">
+'''
+
+
+# A single movie entry html template    
+
+carousel_finished = '''
+    <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
     </div>
 
-    <!-- Main Page Content -->
-    <div class="container">
-      <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-        <div class="container">
-          <div class="navbar-header">
-            <a class="navbar-brand" href="#">Fresh Tomatoes Movie Trailers</a>
+'''
+
+carousel_template = '''
+        <div class="{is_active}" id="{item_id}">
+          <!--<img class="first-slide img-responsive"src="{wide_pic}" alt="First slide">-->
+          <div class="container">
+            <div class="carousel-caption">
+              <h1>{movie_title}</h1>
+              <p>{movie_desc}</p>
+              <p><a class="btn btn-lg btn-primary" href="#" role="button">Watch Trailer</a></p>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
-    <div class="container">
-      {movie_tiles}
-    </div>
+
+'''
+
+sec_two = '''
+
+    <div class="container marketing">
+      <h2>Next Week's Movies: </h2>
+      <!-- Three columns of text below the carousel -->
+      <div class="row">
+'''
+sec_two_template = '''
+        <div class="col-lg-3">
+          <img src="{poster}" alt="{poster_name}"  height="200">
+          <h2>{movie_title}</h2>
+          <p>{movie_desc}</p>
+          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+        </div><!-- /.col-lg-3 -->
+'''
+
+end_page = '''
+    <footer>
+        <p class="pull-right"><a href="#">Back to top</a></p>
+        <p>&copy; 2015 Tyler Harden. </p>
+      </footer>
+
+    </div><!-- /.container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="../static/lib/jquery.min.js"></script>
+    <script src="../static/lib/bootstrap.min.js"></script>
+    <!-- Just to make our placeholder images work. Don't actually copy the next line! -->
+    <script src="../static/assets/js/vendor/holder.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="../static/assets/js/ie10-viewport-bug-workaround.js"></script>
   </body>
 </html>
 '''
 
 
-# A single movie entry html template
-movie_tile_content = '''
-<div class="col-md-6 col-lg-4 movie-tile text-center" data-trailer-youtube-id="{trailer_youtube_id}" data-toggle="modal" data-target="#trailer">
-    <img src="{poster_image_url}" width="220" height="342">
-    <h2>{movie_title}</h2>
-</div>
-'''
-
-
-def create_movie_tiles_content(movies):
+def set_carousel(movies):
     # The HTML content for this section of the page
+    content = ''
+    id = 1
+    is_active = "item active"
+    for movie in movies:
+        # Extract the youtube ID from the url
+        item_id = "item" + str(id)
+        # Append the tile for the movie with its content filled in
+        content += carousel_template.format(
+            is_active = is_active,
+            movie_title=movie.title,
+            item_id=item_id,
+            wide_pic=movie.poster,
+            movie_desc=movie.desc
+        )
+        id += 1
+        is_active = "item"
+    return (main_page_content + content + carousel_finished)
+
+def set_next_week_lineup(movies):
+# The HTML content for this section of the page
     content = ''
     for movie in movies:
         # Extract the youtube ID from the url
-        youtube_id_match = re.search(
-            r'(?<=v=)[^&#]+', movie.trailer_youtube_url)
-        youtube_id_match = youtube_id_match or re.search(
-            r'(?<=be/)[^&#]+', movie.trailer_youtube_url)
-        trailer_youtube_id = (youtube_id_match.group(0) if youtube_id_match
-                              else None)
-
         # Append the tile for the movie with its content filled in
-        content += movie_tile_content.format(
+        content += sec_two_template.format(
             movie_title=movie.title,
-            poster_image_url=movie.poster_image_url,
-            trailer_youtube_id=trailer_youtube_id
+            poster_name=movie.title + "poster",
+            poster=movie.poster,
+            movie_desc=movie.desc
         )
-    return content
+    return (sec_two + sec_two_template + end_page)
+
 
 
 def open_movies_page(movies):
@@ -155,8 +194,7 @@ def open_movies_page(movies):
     output_file = open('fresh_tomatoes.html', 'w')
 
     # Replace the movie tiles placeholder generated content
-    rendered_content = main_page_content.format(
-        movie_tiles=create_movie_tiles_content(movies))
+    rendered_content = set_carousel(movies[:4]) + set_next_week_lineup(movies[4:])
 
     # Output the file
     output_file.write(main_page_head + rendered_content)
--- WesFlix/src/media.py
+++ WesFlix/src/media.py
@@ -2,8 +2,8 @@ class movie:
 	def __init__(self, title, date, poster, desc, trailer):
 		self.title = title
 		self.release_date = date
-		self.poster_image_url = poster
+		self.poster = poster
 		self.desc = desc
-		self.trailer_youtube_url = trailer
+		self.trailer = trailer
 		self.big_pic = None
 
