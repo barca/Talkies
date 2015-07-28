2d5da6c634ba3074e3022a207e0a3ed2cf1bffdd
barca <tharden@wesleyan.edu>
2015-07-28 22:11:18 +0100

    using nano-project to generate static code, in template file

--- WesFlix/__init__.py
+++ WesFlix/__init__.py
@@ -8,12 +8,32 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
-
-@app.route('/template')
-def template():
-  return render_template('template.html')
 @app.route('/contact')
 def contact():
   return render_template('contact.html')
+@app.route('/blade_runner')
+def template():
+  return render_template('Blade Runner.html')
+@app.route('/dirty_harry')
+def template():
+  return render_template('Dirty Harry.html')
+@app.route('/doubt')
+def template():
+  return render_template('Doubt.html')
+@app.route('/five_broken_cameras')
+def template():
+  return render_template('Five Broken Cameras.html')
+@app.route('/futurama')
+def template():
+  return render_template('Futurama.html')
+@app.route('/jurassic_world')
+def template():
+  return render_template('Jurassic World.html')
+@app.route('/shawshank_redemption')
+def template():
+  return render_template('Shawshank Redemption.html')
+@app.route('/the_philadelphia_story')
+def template():
+  return render_template('The Philadelophia Story.html')
 if __name__ == '__main__':
     app.run()
