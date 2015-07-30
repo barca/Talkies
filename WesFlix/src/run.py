eb03834a58b799a4e44eb25c3dfce15be08ff465
barca <tharden@wesleyan.edu>
2015-07-30 15:36:14 +0100

    getting ready for full project

--- WesFlix/src/run.py
+++
@@ -1,9 +0,0 @@
-from entertainment_center import *
-from detail_page import *
-from calendar_page import *
-from fresh_tomatoes import *
-
-
-write_calendar(Movies)
-write_detailed_pages(Movies)
-open_movies_page(Movies)
